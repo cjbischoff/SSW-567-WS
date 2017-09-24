53ef937797f64c01d57addef39a956666fa9c78f
Christopher.Bischoff <christopher.bischoff@phishme.com>
2017-09-24 15:03:18 -0600

    create/homework04 Complete homework04 assignment
    - updated the code to reflect requirements
    - updated unit tests to reflect requirements
    - modify unit test names to reflect traceability matrix
    - readme

---
+++ homework04/README.md
@@ -0,0 +1,47 @@
+# Homework 04: Requirements Testing
+
+## Part 1:  Inspect the triangle requirements below.
+
+
+### Exceptions
+
+- 1.1 contains ambiguity
+- 2.2 contains ambiguity
+- 3.1 unknown terms
+- 4.1 unknown terms
+
+
+### Notes
+
+- 1.1 Doesn't define what kind of number(s) for example whole numbers, integers, etc..
+- 2.2 The requirements is to check for real numbers which is not specific since these are real numbers (1, 15.82, −0.1, 3/4).  I was given a valid lower bound zero, but I cannot quantify "not too big."
+- 3.1 and 4.1 are using letters with no definition of what those letters mean
+
+Since the overall requirements were simple; didn't allow for room for error or several factors didn't apply (was the requirement a repeat in other artifacts or were all stakeholders considered) since I was only handed a list of requirements (i.e no context)
+
+## Part 2:
+
+###  traceability matrix
+
+
+|                                | Test Cases |
+|--------------------------------|------------|
+| Requirements                   |            |
+| 2.1 - Must be greater than 0   | T1         |
+| 2.1 - Must be less than 1000   | T2         |
+| 2.1 - Must be a real number    | T3         |
+| 3.1 - Must be a legal triangle | T4         |
+| 4.1 - If Right triangle        | T5         |
+| 5.1 - If Equilateral triangle  | T6         |
+| 5.1 - If Isosceles triangle    | T7         |
+| 6.1 - Right Isosceles Triangle | T8         |
+| 6.2 - Right Scalene Triangle   | T9         |
+| 6.3 - Equilateral Triangle     | T10        |
+| 6.4 -Isosceles Triangle        | T11        |
+
+#### reference
+
+- equilateral triangles have all three sides with the same length
+- isosceles triangles have two sides with the same length
+- scalene triangles have three sides with different lengths
+- right triangles have three sides with lengths, a, b, and c where a2 + b2 = c2
---
+++ homework04/classify_triangle_tests.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+# coding:utf-8
+
+"""SSW-567-WS: Homework 01: Testing triangle"""
+
+__author__ = """Christopher Bischoff"""
+
+import unittest
+
+from classify_triangle import classify_triangle
+
+
+class ClassifyTriangleTests(unittest.TestCase):
+
+
+    def test_T1_1(self):
+        triangle_object = classify_triangle(0, 1, 1)
+        self.assertEquals(triangle_object.message, 'ERROR:All values must be greater than zero')
+
+    def test_T1_2(self):
+        triangle_object = classify_triangle(1, 0, 1)
+        self.assertEquals(triangle_object.message, 'ERROR:All values must be greater than zero')
+
+    def test_T1_3(self):
+        triangle_object = classify_triangle(1, 1, 0)
+        self.assertEquals(triangle_object.message, 'ERROR:All values must be greater than zero')
+
+
+    def test_T2_1(self):
+        triangle_object = classify_triangle(1001, 1, 1)
+        self.assertEquals(triangle_object.message, 'ERROR:All values must be less than 1000')
+
+    def test_T2_2(self):
+        triangle_object = classify_triangle(1, 1001, 1)
+        self.assertEquals(triangle_object.message, 'ERROR:All values must be less than 1000')
+
+    def test_T2_3(self):
+        triangle_object = classify_triangle(1, 1, 1001)
+        self.assertEquals(triangle_object.message, 'ERROR:All values must be less than 1000')
+
+    def test_T4_1(self):
+        triangle_object = classify_triangle(7, 3, 2)
+        self.assertEquals(triangle_object.message,
+                          'ERROR:Not valid values for Triangle')
+
+    def test_T4_2(self):
+        triangle_object = classify_triangle(2, 4, 2)
+        self.assertEquals(triangle_object.message,
+                          'ERROR:Not valid values for Triangle')
+
+    def test_T4_3(self):
+        triangle_object = classify_triangle(4,5,5e10)
+        self.assertEquals(triangle_object.message, 'ERROR:All values must be less than 1000')
+
+    def test_T5_1(self):
+        triangle_object = classify_triangle(1, 1, 1.4142135623730950488016887241)
+        self.assertEquals(triangle_object.message, 'Right Isosceles Triangle')
+
+    def test_T5_2(self):
+        triangle_object = classify_triangle(10.1, 20.2, 22.584)
+        self.assertEquals(triangle_object.message, 'Right Scalene Triangle')
+
+    def test_T6_1(self):
+        triangle_object = classify_triangle(2, 2, 2)
+        self.assertEquals(triangle_object.message, 'Equilateral Triangle')
+
+    def test_T6_2(self):
+        triangle_object = classify_triangle(10, 10, 10)
+        self.assertEquals(triangle_object.message, 'Equilateral Triangle')
+
+    def test_T11_1(self):
+        triangle_object = classify_triangle(4, 5, 5)
+        self.assertEquals(triangle_object.message, 'Isosceles Triangle')
+
+    def test_T11_2(self):
+        triangle_object = classify_triangle(4, 4, 3)
+        self.assertEquals(triangle_object.message, 'Isosceles Triangle')
+
+    def test_T11_3(self):
+        triangle_object = classify_triangle(10, 10, 2)
+        self.assertEquals(triangle_object.message, 'Isosceles Triangle')
+
+    def test_T11_4(self):
+        triangle_object = classify_triangle(4, 5, 5)
+        self.assertEquals(triangle_object.message, 'Isosceles Triangle')
+
+    def test_T9_1(self):
+        triangle_object = classify_triangle(4, 5, 8)
+        self.assertEquals(triangle_object.message, 'Scalene Triangle')
+
+    def test_T9_2(self):
+        triangle_object = classify_triangle(5, 4, 2)
+        self.assertEquals(triangle_object.message, 'Scalene Triangle')
+
+
+    def test_T9_3(self):
+        triangle_object = classify_triangle(5e-51,4e-51,3e-51)
+        self.assertEquals(triangle_object.message, 'Right Scalene Triangle')
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
